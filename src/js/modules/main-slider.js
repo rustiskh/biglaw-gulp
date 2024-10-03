aea8ba7f7a9948d01f31bf16f2bc5430f23a01a5
Rust <rustam1222@gmail.com>
2024-10-03 18:20:54 +0200

    features block layout (in progress. reviews)

--- src/js/modules/main-slider.js
+++ src/js/modules/main-slider.js
@@ -3,6 +3,7 @@ import { Autoplay, Pagination } from 'swiper/modules';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { tree } from 'gulp';
 
 // init Swiper:
 export const mainSlider = new Swiper('.hero__slider.swiper', {
@@ -29,6 +30,23 @@ export const stepsSlider = new Swiper('.steps__slider-wrapper.swiper', {
         delay: 5000,
     },
     centeredSlides: true,
+    loop: true,
+    spaceBetween: 30,
+    modules: [Pagination, Autoplay],
+});
+
+export const reviewSlider = new Swiper('.review__slider.swiper', {
+    // configure Swiper to use modules
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    autoHeight: true,
     loop: true,
     spaceBetween: 30,
     modules: [Pagination, Autoplay],
